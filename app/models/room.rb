e089a4e5b8c91aefd62010c04939e966aefe43b9
fyurchik <jyrafenun@gmail.com>
2024-09-15 00:53:34 +0300

    Create a UserRoom model to make a participants

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,21 @@
 class Room < ApplicationRecord
   validates_uniqueness_of :name
+  has_many :user_rooms, dependent: :destroy
   scope :public_rooms, -> {where(is_private: false)}
-  after_create_commit { broadcast_append_to "rooms" }
+  after_create_commit { broadcast_if_public }
   has_many :messages
+
+  def broadcast_if_public
+    if is_private == false
+      broadcast_append_to "rooms" 
+    end
+  end
+
+  def self.create_private(users, name)
+   single_room = Room.create(name: name, is_private: true)
+    users.each do |user|
+      single_room.user_rooms.create(user: user, room: single_room)
+    end
+    single_room
+  end
 end
