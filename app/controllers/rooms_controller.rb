308fa91f2f837d21576ba48d5d456f90e324168d
fyurchik <jyrafenun@gmail.com>
2024-09-14 23:39:07 +0300

    Add some actions to rooms and implement turbo load

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,14 +1,23 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user
+  before_action :set_user, only: %i[index]
 
   def index
-    @rooms = Room.public_rooms
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.create(room_params)
   end
 
   private
 
+  def room_params
+    params.require(:room).permit(:name, :is_private)
+  end
+
   def set_user
+    @rooms = Room.public_rooms
     @users = User.all_except(current_user)
   end
 end
