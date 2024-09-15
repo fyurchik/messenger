612b53e5377af913f94063f465f86c46d9ee4335
fyurchik <jyrafenun@gmail.com>
2024-09-15 13:58:29 +0300

    Add avatar to user profile and attached them to a message senders

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,14 @@ class User < ApplicationRecord
   has_many :messages
   scope :all_except, ->(user) { where.not(id: user) }
 
-  after_create_commit { broadcast_append_to "users" }
+  after_create_commit { broadcast_append_to "users"}
+  has_one_attached :image
+  after_commit :set_default_image, on: [:create, :update]
+  private
+
+  def set_default_image
+    unless image.attached?
+      image.attach(io: File.open(Rails.root.join("app", "assets", "images", "default_image.png")), filename: "default_image.png", content_type: "image/png")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,9 @@ Rails.application.routes.draw do
     resources :messages
   end
   root 'pages#home'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  devise_scope :user do
-    get "users", to: "devise/session#new"
-  end
-
+ 
   get "user/:id", to: "users#show", as: :user
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
